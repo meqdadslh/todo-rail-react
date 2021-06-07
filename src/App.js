231b4a29bd48b07e92173497a48332a64f4e5490
Meqdad Ali <meqdadali@Meqdads-MacBook-Pro.local>
2021-06-07 16:46:10 -0400

    build some front end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import AllPosts from "./pages/AllPosts"
+import SinglePost from "./pages/SinglePost"
+import Form from "./pages/Form"
+import {useState, useEffect} from "react"
+import {Route, Switch} from "react-router-dom"
 function App() {
+  /////////////////
+  // Style Objects
+  /////////////////
+  const h1 = {
+    textAlign: "center",
+    margin: "10px"
+  }
+  /////////////////////
+  // State & Other Variables
+  /////////////////////
+  const url = "https://todos-rails-api-ma.herokuapp.com/todos/"
+
+  const [posts, setPosts] = useState([])
+  /////////////////
+  // Functions
+  /////////////////
+  const getTodos = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setPosts(data)
+  }
+  /////////////////
+  // useEffects
+  /////////////////
+  useEffect(() => {getTodos()}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={h1}>My Todo List</h1>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(rp) => <AllPosts {...rp} posts={posts} />}
+        />
+        <Route
+          path="/post/:id"
+          render={(rp) => <SinglePost {...rp} posts={posts} />}
+        />
+        <Route path="/new" render={(rp) => <Form {...rp} />} />
+        <Route path="/edit" render={(rp) => <Form {...rp} />} />
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/pages/AllPosts.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Post from "../components/post"
+
+
+
+const AllPosts = (props) => {
+  return props.posts.map((post) => <Post post={post} key={post.id}/>)
+};
+export default AllPosts;
